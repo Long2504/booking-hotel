3362e8a2fbd033680066295c33967fc2d523453f
long2504 <thanhlong25042001@gmail.com>
2024-06-22 20:44:50 +0700

    set up access response

---
+++ back-end/src/utils/access.response.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const StatusCode = {
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+};
+
+const ReasonStatusCode = {
+    OK: "Success",
+    CREATED: "Created",
+    NO_CONTENT: "No Content",
+};
+
+class SuccessResponse {
+    constructor(
+        message,
+        statusCode = StatusCode.OK,
+        reasonStatusCode = ReasonStatusCode.OK,
+        metaData = {}
+    ) {
+        this.message = !message ? reasonStatusCode : message;
+        this.statusCode = statusCode;
+        this.reasonStatusCode = reasonStatusCode;
+        this.metaData = metaData;
+    }
+
+    send(res, header = {}) {
+        res.status(this.statusCode).json(this);
+    }
+}
+
+class OK extends SuccessResponse {
+    constructor({ message, metaData }) {
+        super({
+            message,
+            metaData,
+        });
+    }
+}
+
+class Created extends SuccessResponse {
+    constructor({
+        message,
+        statusCode = StatusCode.CREATED,
+        reasonStatusCode = ReasonStatusCode.CREATED,
+        metaData,
+    }) {
+        super({
+            message,
+            statusCode,
+            reasonStatusCode,
+            metaData,
+        });
+    }
+}
+
+export { OK, Created };
