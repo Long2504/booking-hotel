218cb861a124949bda7198c0c3e519618fd0b929
Long2504 <thanhlong25042001@gmail.com>
2024-06-27 02:48:04 +0700

    update layout client

--- front-end/src/layouts/ClientLayout.jsx
+++ front-end/src/layouts/ClientLayout.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { generateRoute } from "../utils/common.utils";
 import routesClient from "../router/client.router";
 import NotFound from "../pages/core/NotFound";
-import HeaderClient from "../components/client/Header.client";
+import HeaderClient from "../components/client/core/Header.client";
 import { Layout } from "antd";
 const { Content, Footer } = Layout;
 function ClientLayout() {
