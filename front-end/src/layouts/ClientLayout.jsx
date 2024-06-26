af7b418e971cc68a6e4f056ebc5bf25f51e26b98
Long2504 <thanhlong25042001@gmail.com>
2024-06-26 21:56:54 +0700

    set layout client

--- front-end/src/layouts/ClientLayout.jsx
+++ front-end/src/layouts/ClientLayout.jsx
@@ -2,15 +2,28 @@ import { Routes, Route } from "react-router-dom";
 import { generateRoute } from "../utils/common.utils";
 import routesClient from "../router/client.router";
 import NotFound from "../pages/core/NotFound";
-
+import HeaderClient from "../components/client/Header.client";
+import { Layout } from "antd";
+const { Content, Footer } = Layout;
 function ClientLayout() {
+    const layoutStyle = {
+        borderRadius: 8,
+        with: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+    };
     return (
-        <div>
-            <Routes>
-                {generateRoute(routesClient, Route)}
-                <Route path={"*"} element={<NotFound />} />
-            </Routes>
-        </div>
+        <Layout style={layoutStyle}>
+            <HeaderClient />
+            <Content>
+                <Routes>
+                    {generateRoute(routesClient, Route)}
+                    <Route path={"*"} element={<NotFound />} />
+                </Routes>
+            </Content>
+            <Footer />
+        </Layout>
     );
 }
 
