103ceeb417d1ba7d834d7e6b475af39d49a6bead
Long2504 <thanhlong25042001@gmail.com>
2024-06-26 16:54:58 +0700

    getListByListId of SubExtensionService

--- back-end/src/services/subExtension.service.js
+++ back-end/src/services/subExtension.service.js
@@ -3,6 +3,17 @@ import BaseService from "./base.service.js";
 import { Op } from "sequelize";
 
 class SubExtensionService extends BaseService {
+    async getListByListId(listId) {
+        const options = {
+            where: {
+                id: {
+                    [Op.in]: listId,
+                },
+            },
+            attributes: ["id", "name"],
+        };
+        return await this.getAll(options);
+    }
 }
 
 export default new SubExtensionService(SubExtensionModel);
