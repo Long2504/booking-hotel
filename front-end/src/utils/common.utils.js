421417cdadc3f5d48f63b1a390c4c81ca7f09c45
Long2504 <thanhlong25042001@gmail.com>
2024-06-27 22:46:59 +0700

    add function format VND

--- front-end/src/utils/common.utils.js
+++ front-end/src/utils/common.utils.js
@@ -1,18 +1,25 @@
 function generateRoute(AppRoutes, Route) {
-    return AppRoutes.map((r, i) => {
-        let element = undefined;
-        if (r.element) {
-            element = r.element;
-        }
-        if (r.children) {
-            return (
-                <Route key={i} {...r} element={element}>
-                    {generateRoute(r.children, Route)}
-                </Route>
-            );
-        }
-        return <Route key={i} {...r} element={element} />;
-    });
+	return AppRoutes.map((r, i) => {
+		let element = undefined;
+		if (r.element) {
+			element = r.element;
+		}
+		if (r.children) {
+			return (
+				<Route key={i} {...r} element={element}>
+					{generateRoute(r.children, Route)}
+				</Route>
+			);
+		}
+		return <Route key={i} {...r} element={element} />;
+	});
 }
 
-export { generateRoute };
+function vietNamDong(money) {
+	return Intl.NumberFormat("vi", {
+		style: "currency",
+		currency: "VND",
+	}).format(money);
+}
+
+export { generateRoute, vietNamDong };
