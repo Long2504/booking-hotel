c162652a78ef395b380ee6157feea96a71ac8e49
Long2504 <thanhlong25042001@gmail.com>
2024-06-28 23:44:25 +0700

    fix class to className of NotFound

--- front-end/src/pages/core/NotFound.jsx
+++ front-end/src/pages/core/NotFound.jsx
@@ -1,20 +1,20 @@
 function NotFound() {
-  return (
-    <div id="notfound" className="notfound">
-      <div class="notfound__bg">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div class="notfound__content">
-        <div class="notfound__content__404">
-          <h1>404</h1>
-        </div>
-        <h2>Trang không tồn tại</h2>
-      </div>
-    </div>
-  );
+	return (
+		<div id='notfound' className='notfound'>
+			<div className='notfound__bg'>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+			<div className='notfound__content'>
+				<div className='notfound__content__404'>
+					<h1>404</h1>
+				</div>
+				<h2>Trang không tồn tại</h2>
+			</div>
+		</div>
+	);
 }
 
 export default NotFound;
