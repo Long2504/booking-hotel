dfb60a6480ef0c693a5f29a45588374d1401f83f
Long2504 <thanhlong25042001@gmail.com>
2024-06-28 23:43:48 +0700

    add GoogleOAuthProvider and clientId

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -6,13 +6,16 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <Router>
-                <App />
-            </Router>
-        </Provider>
-    </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<Router>
+				<GoogleOAuthProvider clientId='1059722329790-ggq09ntvloiqd194dgij5ahre7su77l0.apps.googleusercontent.com'>
+					<App />
+				</GoogleOAuthProvider>
+			</Router>
+		</Provider>
+	</React.StrictMode>
 );
