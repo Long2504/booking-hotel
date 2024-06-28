17b699141cb798704daf4416df503807f6481e8c
Long2504 <thanhlong25042001@gmail.com>
2024-06-28 23:42:10 +0700

    add UI input core

---
+++ front-end/src/components/core/input.core.js
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+
+function InputCore({
+	register,
+	type,
+	name,
+	label,
+	placeholder,
+	value,
+	readOnly,
+	error,
+}) {
+	console.log(error);
+	const registerInput = register ? register(name) : {};
+	return (
+		<div className='input-core'>
+			<label>{label}</label>
+			<input
+				{...registerInput}
+				id={name}
+				readOnly={readOnly}
+				type={type || "text"}
+				name={name}
+				defaultValue={value || ""}
+				placeholder={placeholder}
+			/>
+			<p className='input-core__error'>{error?.message}</p>
+		</div>
+	);
+}
+
+export default InputCore;
