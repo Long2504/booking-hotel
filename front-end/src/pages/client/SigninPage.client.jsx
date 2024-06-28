0b9d16e2afbea0f9e5e5ae177f0f40b8aa25df99
Long2504 <thanhlong25042001@gmail.com>
2024-06-29 00:11:37 +0700

    signin page ui

--- front-end/src/pages/client/SigninPage.client.jsx
+++ front-end/src/pages/client/SigninPage.client.jsx
@@ -1,9 +1,65 @@
-export function SigninPage() {
-    return (
-        <div>
-            <h1>Signin Page</h1>
-        </div>
-    )
+import { GoogleLogin } from "@react-oauth/google";
+import { Divider } from "antd";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import InputCore from "../../components/core/input.core";
+import signinFormSchema from "../../validate/signin.validate";
+
+function SigninPage() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(signinFormSchema),
+	});
+	console.log(errors);
+	return (
+		<div className='signin-client'>
+			<div className='signin-client__content'>
+				<h6>Sign in</h6>
+				<InputCore
+					label={"Email"}
+					placeholder={"Email"}
+					name={"email"}
+					register={register}
+					error={errors.email}
+				/>
+				<InputCore
+					label={"Password"}
+					placeholder={"Password"}
+					name={"password"}
+					register={register}
+					error={errors.password}
+					type='password'
+				/>
+
+				<button
+					className='signin-client__content__submit'
+					onClick={handleSubmit((data) => {
+						console.log(data);
+					})}
+				>
+					Đăng nhập
+				</button>
+				<Divider plain>đăng nhập với</Divider>
+
+				<div className='signin-client__content__login-with-gg'>
+					<GoogleLogin
+						onSuccess={() => {}}
+						onError={() => {
+							console.log("Login Failed");
+						}}
+					/>
+				</div>
+				<p className='signin-client__content__by-signing'>
+					By signing in, I agree to Systems Inc. Terms of Use and
+					Privacy Policy.
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default SigninPage;
