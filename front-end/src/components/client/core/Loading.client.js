21722463412203208e867385a6d63b256e68021e
Long2504 <thanhlong25042001@gmail.com>
2024-06-29 20:23:30 +0700

    loading UI client

---
+++ front-end/src/components/client/core/Loading.client.js
@@ -0,0 +1,18 @@
+function Loading() {
+	return (
+		<div className='loading-hotel'>
+			<div className='loading-hotel__content'>
+				<img
+					src='https://cdn6.agoda.net/images/brand/agoji-parachute.gif'
+					alt='gif1'
+				></img>
+				<h3>BEST PRICE GUARANTEE</h3>
+				<h5>
+					We'll match any price on the web or refund the difference
+				</h5>
+			</div>
+		</div>
+	);
+}
+
+export default Loading;
