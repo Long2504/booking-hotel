dcce209a6011bb7cf32e5d01204e82c7a0eab395
Long2504 <thanhlong25042001@gmail.com>
2024-06-30 21:10:33 +0700

    UI scroll up nav

--- front-end/src/components/client/core/ScrollUpNav.client.js
+++ front-end/src/components/client/core/ScrollUpNav.client.js
@@ -1,24 +1,18 @@
 import React, { useEffect, useState } from "react";
-import SearchIcon from "@mui/icons-material/Search";
-import { Calendar } from "../FormSelection/Calendar";
-import RoomSelect from "../FormSelection/RoomSelect";
+import { SearchOutlined } from "@ant-design/icons";
+import Calendar from "./Calendar.client";
+import RoomSelect from "./RoomSelect.client";
 import { Input } from "antd";
-import { useDispatch, useSelector } from "react-redux";
-import { setFilterHotel } from "../../../../redux/slice/hotelSlice.host";
-import { useSearchParams } from "react-router-dom";
 
-function ScrollUpNav({ calcScroll, handleClicking }){
+function ScrollUpNav({ calcScroll, handleClicking }) {
 	const [scroll, setScroll] = useState(0);
 	const [focus, setFocus] = useState(false);
 
-	const { filter } = useSelector((state) => state.hotelHost);
-	const dispatch = useDispatch();
 	useEffect(() => {
 		window.addEventListener("scroll", (e) => {
 			setScroll(window.scrollY);
 		});
 	});
-	const [searchParams] = useSearchParams();
 
 	const styleScrollUpNav = {
 		position: "fixed",
@@ -35,26 +29,20 @@ function ScrollUpNav({ calcScroll, handleClicking }){
 				<Input
 					className='scroll-up-nav__content__search'
 					typeof='Search'
-					prefix={<SearchIcon />}
+					prefix={<SearchOutlined />}
 					onFocus={() => setFocus(true)}
 					onBlur={() => setFocus(false)}
-					value={
-						filter.searchQuery || searchParams.get("searchQuery")
-					}
-					onChange={(e) =>
-						dispatch(
-							setFilterHotel({ searchQuery: e.target.value })
-						)
-					}
 				/>
 
 				<div className='scroll-up-nav__content__calendar'>
-					<Calendar setFocus={setFocus} filter={filter} />
+					<Calendar setFocus={setFocus} />
 				</div>
 				<div className='scroll-up-nav__content__room-select'>
-					<RoomSelect filterNumber={filter} setFocus={setFocus} />
+					<RoomSelect setFocus={setFocus} />
 				</div>
 			</div>
 		</div>
 	);
-};
+}
+
+export default ScrollUpNav;
