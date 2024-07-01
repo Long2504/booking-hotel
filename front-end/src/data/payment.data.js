d412642d4780daa80f1a9c25afc3522528dc8ec4
Long2504 <thanhlong25042001@gmail.com>
2024-07-01 23:46:54 +0700

    add payment data

---
+++ front-end/src/data/payment.data.js
@@ -0,0 +1,119 @@
+export const dataPayment = {
+  "hotel": {
+    "id": "008e325e-be93-499f-be3b-e28182b06827",
+    "name": "Ruby Can Tho Hotel",
+    "address": "34 Nguyen An Ninh, Bến Ninh Kiều, Cần Thơ, Việt Nam",
+    "star": 2,
+    "images": [
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/213581877.jpg?k=10570d076a3927719c4124659d84c271448e4caa0928c9bc4d52e0acdf167d74&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/90035796.jpg?k=14152421fdbdfe0592385d0b4096dd34113a2b6ca1390af106ea5b909da334e9&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/213582273.jpg?k=98cd3ad7494962a98336d677b01f1082e4bfb6f7d9aaf8c6d8385f42b75b2558&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/198869302.jpg?k=45bc0c111edb8caf3f38637f2193f2b13ed860359275eddaf2595cdf91b9feab&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/90036141.jpg?k=34f85317a90800b3187b4ac2ccaa1e7b575294f52b9446a7fbcaf153c0663442&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/198869655.jpg?k=e2f2917fc2ae12e7cf1f8d445ae547c56bbc52ea343943c9bd9c1b89a104a44c&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/90044843.jpg?k=99b29b506b53d561991b08e60c44414a65fc058a4b3302a3ee6fd8565fb649a9&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/90035821.jpg?k=9bc4e6de0fc9ba98c699d761398d9dcfe888e1c0a56a478ebd3d5f5e72c2c636&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/198870168.jpg?k=7da69bc775764952da6625da3ed82d4fa03ae3a03b9f2fca0853b418b5827a80&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178186201.jpg?k=b3e41ef8b03b84c3ec5bf7e7fc869cca16ec8896c054a4d974a8ff86d6d54c52&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/90044797.jpg?k=7955534e4797306021136df1efaec2148a03a22e800f837e56b531f3d2f059b2&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/90044839.jpg?k=9f1727d30a2816db404730123265c59b312331bf22485b4a2c5151a0196d521b&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/90035820.jpg?k=d79949e9e20b588c4d2f8f99940ec0cfd2ceb32c2061fadb34a914fc9922a41f&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/90044815.jpg?k=e527e11fde7db6a1a9b0120e0b45fd298aa9d874a2ca7fa8898b9fd2b16bcf50&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/541271872.jpg?k=c17c7eca5587e37d11875d5cf25bae3b18e9ecb4dc07e9b0759dc970894fcb10&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178185928.jpg?k=16defc9fd37c36adea30bf2098a2d51cb622f409ba06405b1c61f0dbacffb7f6&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/198870229.jpg?k=6e463c3ef073d673090293142269fe1f5bee43dd64e1347e70a4c601cbdfe490&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178185346.jpg?k=be1f6730f8f9635f84bd5cf7df73ad419451bb560afa44d7e7d623e1719eddc0&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/213587595.jpg?k=c3057e62a1a5b93dfd334db13e89f8900ca47f85a889269d11ecfb8f9ad0d082&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/90035816.jpg?k=d1bcf02e00a79e524b704d89362b51ddc25886e1e0f70fa0adc70438e0a113d1&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/90035815.jpg?k=7599227f979613f13c75845cbc451bb2b2db38c99cec65e2b88a0aa2685e7673&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/90035822.jpg?k=02996e60bb56da23d91290248b40a54df86835b2caf423cfd6b418b1073ac0ef&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178187014.jpg?k=38e0e7f617eeb1cd413a3a6655d46783cd51c46ebc1e79f073283cc3fff36a7f&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178187728.jpg?k=62801a20b8ef12efe3cbc1e5f054d4a7a368bcf4a3c06f3a81e396e37c3aab16&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178187478.jpg?k=811164d349df1b736422bd91126e5c830f59a9cb9f649f047f3971ea1e0b0e1b&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/213581909.jpg?k=c9f4b9f3677945c47c2c1726c22ba90fae2deb7de7ad8e12e8a796a377a4189a&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/90044919.jpg?k=ce60c7143584faa9748b815d63baf4489807d3d16819198d3beb03057d420911&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/90044437.jpg?k=e539df399a1de7a0e468c3e7145ac4096a7afeccc26095fe933c7d798221cfc8&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178186676.jpg?k=0cc2ce229b12bbbc66be973f6408563344a66f1eb03ddcafd7b191c1904ad674&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178186534.jpg?k=a5b6321254a3f1e4ddff5e747991c5cb0c9d65acaba3e0931717111dcebf3f92&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178187930.jpg?k=a6d1820f0f9d64f95f8f073d4b42faceaf826b58ce14638d56b274f1c56de617&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/198870335.jpg?k=8470e6520156889fe0693830eb36df4ad5d4f2a252dfb88bfd796ac6552eb530&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178186124.jpg?k=ecf521f90828fdbc4733d8feaffd432ab33157d526610b766c8c14f552f5a8a0&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/198870465.jpg?k=b70d52500f1ede7e063b8cc43da5fd67d450c37695de14276e13bbdcad1bed24&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/198870110.jpg?k=0215a20be3a3b3d6147a28b6c4df6af5a7a7b94ac3cfa13e069a11503af7cecb&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/90035827.jpg?k=540d0605cf38e79371be8ae885e27e209a7ceb52ed5fa62285a284c6a17ba90e&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178187648.jpg?k=b6938176339647e14842d91c287b6cfd987072d0c4ab6dcafe98321ad34d452b&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178185853.jpg?k=737f860ea9cbad6d5bef9a59263ecaf0552a8c6a1ecad42d1d347cf24a03f1c9&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/257252445.jpg?k=a069c0931fafd3b77ddf0cfecb52e8c85dcbfe955cbeb58767cf28a524793b5c&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178187436.jpg?k=261aed5d023270b35b95c2ef3b7abce4bc6aa9799e320b022a1e6986fda9ab3e&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/198869429.jpg?k=1657554154ea2bfb448af33b080866d2f24d5bdf83899001f0b0d490f4a443f5&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/213587551.jpg?k=09c2ac7b8d26f51102f638ca55dcf5bcc32a5cc152981a8f94c440ca6aee6556&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178187370.jpg?k=e50509ca1d2024d573cd8ed487e4566d4c6bdbf772e7751b02fbd92d370e39bb&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/90035823.jpg?k=7e8ac0fe992a67abb22c70c84f6d8f736175f38b9e13024e4529dc679c1a392b&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178187843.jpg?k=5d7e5dc415e52a228d82d92591400e608994ecfe3dde2392ebc13e4df41711d8&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178187889.jpg?k=fbeac44fecafbcf91b5e44682ed3ae24b193bddda2504228cd1773ed02f946e7&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/90035830.jpg?k=d5cb0c588a64ad69723ad0d40305c04eb5f6605c8a307b30536830dd40c37fb4&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/90035832.jpg?k=a2d9f2cd19ff0b61d9efedc24f6325c2725df72e83404a2da70c69f7d136230b&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178186033.jpg?k=d8d7e94aefc9783732b7d27b34c26e36ab48bdcb03396f90153336b8e228aace&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/213588343.jpg?k=a95e5e80953b94830e6d013696b17847947d8a02fcfa1f5b797ef38651f9f374&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178185765.jpg?k=43fe4d89b85b557f99e97e2f63cb4000e0f8f390569e4b22e463dfbb1092bbf5&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178187793.jpg?k=ee55f1edf3ea4e5666db19b8fec378b48d6f713c7d241bd7a4570d217736a006&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/257252497.jpg?k=3c33d151b24f8e90747c2e8bddf0b08dbd647ce9c0f05f9ac7051094219455ff&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/198869998.jpg?k=84ae97dd770f99166d3d48d226e44caa331e3eb1ad34bfbdb8f84b76316cd4fe&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178186899.jpg?k=16a25cecb60583962f35c2ad4a3f4e43f91f4b0c6dfa88290984eb477ac528fa&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178187613.jpg?k=7810abe42b2a7414a5afb569e90390d57c6731b589084f94540f302bb556facd&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/198870063.jpg?k=044ab0b7b48afa61de8fbaaa16dd202d812746a1a598ed02e5246187422c7f7a&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178186325.jpg?k=a2331988c9cae5b81369de8bfe438eaee8b9ef2f716c732f0d8d032e06f7eafc&o=",
+      "https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/178187313.jpg?k=6e22c57f4a214b51901c94a271592c3c29533547c9b32dbd2bb685311d17706a&o="
+    ]
+  },
+  "room": {
+    "id": "0fb722eb-ccfc-4876-a247-cb20a1f3a704",
+    "area": 26,
+    "bathrooms": 1,
+    "occupancy": 4,
+    "number": "1",
+    "price": 3462940,
+    "images": [
+      "https://q-xx.bstatic.com/xdata/images/hotel/max500/198869429.jpg?k=1657554154ea2bfb448af33b080866d2f24d5bdf83899001f0b0d490f4a443f5&o="
+    ],
+    "roomType": {
+      "id": "44b57aee-112b-493f-ad94-2e3d2281ef50",
+      "name": "Phòng Hai Giường Đơn (Twin Room)"
+    },
+    "beds": [
+      {
+        "id": "f90a9b39-e207-487d-896a-ef1a2d711e13",
+        "number": 2,
+        "bedType": {
+          "id": "0849ffcc-7adc-42a0-bd6c-86efa4bd22d4",
+          "name": "Giường đôi"
+        }
+      }
+    ],
+    "totalDays": 1,
+    "peopleNumber": "2"
+  },
+  "customer": {
+    "id": "",
+    "displayName": "",
+    "email": "",
+    "phoneNumber": "",
+    "address": {
+      "id": "",
+      "city": "",
+      "district": "",
+      "ward": "",
+      "codeCity": "",
+      "codeDistrict": "",
+      "codeWard": "",
+      "street": ""
+    }
+  },
+  "checkInDate": "2024-06-30",
+  "checkOutDate": "2024-07-01",
+  "totalPrice": 3809234.0000000005,
+  "note": {
+    "contentOne": "",
+    "contentTwo": ""
+  }
+}
