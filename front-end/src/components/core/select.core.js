bdcea5fde74abfa9fdfcddf8d2a493349cc402ec
Long2504 <thanhlong25042001@gmail.com>
2024-07-01 23:41:50 +0700

    custom select core

---
+++ front-end/src/components/core/select.core.js
@@ -0,0 +1,24 @@
+import { Select } from "antd";
+
+function SelectCore({ data, label, placeholder, width, height }) {
+	return (
+		<div
+			className='select-core'
+			style={{
+				width: width || "100%",
+				height: height || "100px",
+			}}
+		>
+			<label>{label}</label>
+			<Select
+				className='select-core__input'
+				showSearch
+				placeholder={placeholder}
+				optionFilterProp='children'
+				options={data}
+			/>
+		</div>
+	);
+}
+
+export default SelectCore;
