7e55c3415f37579b95c92bd3361d5e6c9420ad4b
Long2504 <thanhlong25042001@gmail.com>
2024-07-06 17:17:14 +0700

    handle error fe

--- front-end/src/utils/common.utils.js
+++ front-end/src/utils/common.utils.js
@@ -1,3 +1,5 @@
+
+
 function generateRoute(AppRoutes, Route) {
 	return AppRoutes.map((r, i) => {
 		let element = undefined;
@@ -22,4 +24,24 @@ function vietNamDong(money) {
 	}).format(money);
 }
 
-export { generateRoute, vietNamDong };
+function handleError(error) {
+	let errorCode = "UNKNOWN_ERROR";
+	let errorMessage = "An unknown error occurred";
+
+	if (error.response) {
+		// Server responded with a status other than 200 range
+		errorCode = error.response.status;
+		errorMessage = error.response.data.message || error.response.statusText;
+	} else if (error.request) {
+		// Request was made but no response was received
+		errorCode = "NO_RESPONSE";
+		errorMessage = "No response received from server";
+	} else {
+		// Something happened in setting up the request that triggered an Error
+		errorCode = "REQUEST_SETUP_ERROR";
+		errorMessage = error.message;
+	}
+	return { errorCode, errorMessage };
+}
+
+export { generateRoute, vietNamDong, handleError };
