4fa639c3cdf0cca2736575ac4794e931505fefd0
Long2504 <thanhlong25042001@gmail.com>
2024-07-07 17:21:28 +0700

    add isAuthenticated to header fe

--- front-end/src/components/client/common/Header.client.js
+++ front-end/src/components/client/common/Header.client.js
@@ -2,51 +2,107 @@
 import { logoMain } from "../../../assets/images/index.image";
 
 //libs
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Header } from "antd/es/layout/layout";
-import { Button, Space } from "antd";
+import { Avatar, Button, Dropdown, Space } from "antd";
+import { useSelector } from "react-redux";
+
+//icons
+import { LuUser2 } from "react-icons/lu";
+import { FaSortDown } from "react-icons/fa6";
+
 function HeaderClient() {
-    return (
-        <Header className="header-client">
-            <Link to="/">
-                <div className="header-client__left">
-                    <img
-                        className="header-client__left__header-logo"
-                        src={logoMain}
-                        alt=""
-                    />
-                </div>
-            </Link>
-            <div className="header-client__right">
-                <Space size={14}>
-                    <Link to="/host/overview" target="_blank">
-                        <Button
-                            className="header-client__right__btn-host"
-                            size="large"
-                        >
-                            Đăng ký cho thuê khách sạn
-                        </Button>
-                    </Link>
-                    <Link to="/signin">
-                        <Button
-                            className="header-client__right__btn-signin"
-                            size="large"
-                        >
-                            Đăng nhập
-                        </Button>
-                    </Link>
-                    <Link to="/signup">
-                        <Button
-                            className="header-client__right__btn-signup"
-                            size="large"
-                        >
-                            Tạo tài khoản
-                        </Button>
-                    </Link>
-                </Space>
-            </div>
-        </Header>
-    );
+	const { isAuthenticated, user } = useSelector((state) => state.auth);
+	const navigate = useNavigate();
+	const itemsDropdown = [
+		{
+			key: "1",
+			label: "Danh sách đặt phòng",
+			onClick: () => {
+				navigate("/");
+			},
+		},
+		{
+			key: "2",
+			label: "Hồ sơ cá nhân",
+			onClick: () => {
+				navigate("/");
+			},
+		},
+		{
+			key: "3",
+			label: "Đăng ký cho thuê khách sạn",
+		},
+		{
+			key: "4",
+			label: "Thoát",
+			onClick: () => {},
+		},
+	];
+	return (
+		<Header className='header-client'>
+			<Link to='/'>
+				<div className='header-client__left'>
+					<img
+						className='header-client__left__header-logo'
+						src={logoMain}
+						alt=''
+					/>
+				</div>
+			</Link>
+			<div className='header-client__right'>
+				<Space size={14}>
+					<Link to='/host/overview' target='_blank'>
+						<Button
+							className='header-client__right__btn-host'
+							size='large'
+						>
+							Đăng ký cho thuê khách sạn
+						</Button>
+					</Link>
+					{isAuthenticated ? (
+						<Space className='header-client__right__user'>
+							<Avatar icon={<LuUser2 />} src={user?.photoUrl} />
+							<Dropdown
+								menu={{
+									items: itemsDropdown,
+								}}
+								trigger={["click"]}
+								overlayStyle={{ top: "70px" }}
+								align={"right"}
+							>
+								<Space direction='horizontal'>
+									<span>
+										{user?.displayName || user?.email}
+									</span>
+									<FaSortDown size={16} />
+								</Space>
+							</Dropdown>
+						</Space>
+					) : (
+						<>
+							<Link to='/signin'>
+								<Button
+									className='header-client__right__btn-signin'
+									size='large'
+								>
+									Đăng nhập
+								</Button>
+							</Link>
+							<Link to='/signup'>
+								<Button
+									className='header-client__right__btn-signup'
+									size='large'
+								>
+									Tạo tài khoản
+								</Button>
+							</Link>
+						</>
+					)}
+				</Space>
+			</div>
+		</Header>
+	);
 }
 
 export default HeaderClient;
