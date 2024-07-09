b8235413136c690535e0efa82eb545d9d2168968
Long2504 <thanhlong25042001@gmail.com>
2024-07-09 23:07:24 +0700

    logo image

--- front-end/src/assets/images/index.image.js
+++ front-end/src/assets/images/index.image.js
@@ -1,5 +1,9 @@
+//image client
 import logoMain from "./logo-main.png";
 import noImage from "./no-image.png";
 import paypalImage from "./paypal.png";
 
-export { logoMain, noImage, paypalImage };
+//image admin
+import logoAdmin from "./logo-admin.png";
+
+export { logoMain, noImage, paypalImage, logoAdmin };
