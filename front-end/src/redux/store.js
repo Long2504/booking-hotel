1c4e784d976dfec21fdbc96faa280cd6e9741b89
Long2504 <thanhlong25042001@gmail.com>
2024-07-10 23:13:12 +0700

    slice global loading fe

--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slice/authslice.redux";
+import globalLoadingReducer from "./slice/globalLoading.redux";
 export const store = configureStore({
 	reducer: {
 		auth: authReducer,
+		globalLoading: globalLoadingReducer,
 	},
 });
