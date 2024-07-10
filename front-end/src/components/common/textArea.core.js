6b42d65ca7bb02d1b7ab90c614c5da1738d802a1
Long2504 <thanhlong25042001@gmail.com>
2024-07-10 23:14:23 +0700

    text area core fe

---
+++ front-end/src/components/common/textArea.core.js
@@ -0,0 +1,30 @@
+function TextAreaCore({
+	label,
+	placeholder,
+	width,
+	height,
+	register,
+	name,
+	rows,
+}) {
+	const registerInput = register ? register(name) : {};
+	return (
+		<div
+			className='text-area-core'
+			style={{
+				width: width || "100%",
+				height: height || "100%",
+			}}
+		>
+			<label>{label}</label>
+			<textarea
+				placeholder={placeholder}
+				{...registerInput}
+				name={name}
+				rows={rows}
+			/>
+		</div>
+	);
+}
+
+export default TextAreaCore;
