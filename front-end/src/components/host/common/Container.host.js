abbb9242df79d7757e44cbd1af135e461685e469
Long2504 <thanhlong25042001@gmail.com>
2024-07-12 22:47:48 +0700

    container host fe

---
+++ front-end/src/components/host/common/Container.host.js
@@ -0,0 +1,22 @@
+import Box from "../../common/box.core";
+
+function ContainerHost({ className, style, children, ...props }) {
+	return (
+		<Box
+			className={className}
+			style={{
+				width: "100%",
+				marginTop: "20px",
+				backgroundColor: "white",
+				padding: "15px",
+				...style,
+			}}
+			radius={6}
+			{...props}
+		>
+			{children}
+		</Box>
+	);
+}
+
+export default ContainerHost;
