1bf1ef737613b268120290ddd76dde17dbaf9669
Long2504 <thanhlong25042001@gmail.com>
2024-07-13 16:45:59 +0700

    list extension host fake data fe

---
+++ front-end/src/data/listExtensionHost.data.js
@@ -0,0 +1,130 @@
+const listExtensionHost = [
+	{
+		id: "4b749464-a028-4eac-b9f4-4a5fe2289e6f",
+		name: "Giải trí",
+		description:
+			"Không phải lúc nào khách cũng muốn rời khỏi chỗ nghỉ, các tiện nghi này cho phép họ ở trong chỗ nghỉ và thấy vui",
+		subExtensions: [
+			{
+				id: "061c8c5a-12a1-4e86-b91b-78077abe03f4",
+				name: "TV",
+			},
+			{
+				id: "2a8fd76d-8392-495d-bf95-a0e967cf6f04",
+				name: "Truyền hình cáp",
+			},
+			{
+				id: "6b955280-f78f-4d3a-8ed9-7b5ddd1e0f59",
+				name: "Internet",
+			},
+			{
+				id: "febbe8f1-966f-4186-9259-d8c9ef0b17be",
+				name: "Wi-Fi",
+			},
+		],
+	},
+	{
+		id: "48bc0c63-b555-43c4-b613-e7b145e8609c",
+		name: "Bếp",
+		description:
+			"Khách thường đặt nhà vì có bếp, hãy đảm bảo rằng quý đối tác cho du khách biết nên mong đợi gì trong không gian bếp",
+		subExtensions: [
+			{
+				id: "12cffb45-1b3a-4be6-9311-1adbef43b230",
+				name: "Máy rửa bát",
+			},
+			{
+				id: "7ba0bd39-5cee-480c-b9dc-e145159d523b",
+				name: "Bếp",
+			},
+			{
+				id: "a5218faf-652a-47c0-a8ad-e33df2abcbd7",
+				name: "Lò nướng",
+			},
+			{
+				id: "bc36ed63-390c-4471-ba5e-350861e2937b",
+				name: "Đồ dùng cơ bản trong nhà bếp (dụng cụ nấu ăn, dầu, nước sốt)",
+			},
+			{
+				id: "dd835a4b-f9c6-43a7-8397-88e4d3444e50",
+				name: "Lò vi sóng",
+			},
+			{
+				id: "f2bcaa6c-667e-45e2-bdc9-9991ab4fad9d",
+				name: "Tủ lạnh",
+			},
+			{
+				id: "fee99af6-e0cf-43fd-a1a8-77575d3f9d67",
+				name: "Bát đĩa và dụng cụ ăn uống",
+			},
+		],
+	},
+	{
+		id: "9158ac8a-3bb3-4303-b920-2c156ab17e5a",
+		name: "Độ an toàn và sạch sẽ",
+		description:
+			"Cho khách thấy rằng quý đối tác coi trọng sự an toàn và sức khỏe của họ với các tiện nghi này.",
+		subExtensions: [
+			{
+				id: "cee6a438-a6d5-418c-876f-bed912adf2b0",
+				name: "Nhận phòng/trả phòng không tiếp xúc",
+			},
+		],
+	},
+	{
+		id: "1dd0665c-e938-4ce9-9e49-efbb2311d42d",
+		name: "Phòng tắm",
+		description: "Cho khách biết (các) phòng tắm có đặt trưng gì",
+		subExtensions: [
+			{
+				id: "0aa72698-f033-4dbb-8e38-dc78f18d58dc",
+				name: "Sữa tắm",
+			},
+			{
+				id: "8c4303da-9d1c-435e-9846-b7f4335d6021",
+				name: "Bồn tắm",
+			},
+		],
+	},
+	{
+		id: "a26f789b-62f9-4609-99ec-c2d8d4ab97b1",
+		name: "Ngoài trời",
+		description:
+			"Cho du khách thấy những loại trải nghiệm ngoài trời họ có thể mong đợi",
+		subExtensions: [
+			{
+				id: "3eec5d87-ebea-4eb7-a7c7-eef6844d471c",
+				name: "Ban công hoặc sân hiên",
+			},
+			{
+				id: "49c324b4-dafd-413a-9a1d-527371d13f8f",
+				name: "Vườn hoặc sân sau",
+			},
+			{
+				id: "a24bfa2c-08a0-410f-bca0-c70f5d860549",
+				name: "Khu đối diện bãi biển",
+			},
+			{
+				id: "fb3e581c-6cf5-40b3-b806-068605c11245",
+				name: "Xe đạp",
+			},
+		],
+	},
+	{
+		id: "ffac895b-a9fc-4904-86f3-8b3b67567e72",
+		name: "Elegance Restaurant",
+		description: "",
+		subExtensions: [
+			{
+				id: "8613378e-faa9-480e-b4e0-bf408f5913ed",
+				name: "Ẩm thực: Mỹ, món Á, quốc tế",
+			},
+			{
+				id: "a0ef04db-5c90-4870-8adc-fc3db539453e",
+				name: "Thực đơn: gọi theo món",
+			},
+		],
+	},
+];
+
+export default listExtensionHost;
