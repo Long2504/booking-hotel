1695b17b65aadfae3110e8c6a922cde9edcd9af5
Long2504 <thanhlong25042001@gmail.com>
2024-07-19 21:50:23 +0700

    generrateAddress, decodeAddress fe

--- front-end/src/utils/common.utils.js
+++ front-end/src/utils/common.utils.js
@@ -1,4 +1,4 @@
-
+import { districts, provinces, wards } from "./dataAddress.utils";
 
 function generateRoute(AppRoutes, Route) {
 	return AppRoutes.map((r, i) => {
@@ -44,4 +44,39 @@ function handleError(error) {
 	return { errorCode, errorMessage };
 }
 
-export { generateRoute, vietNamDong, handleError };
+function generateAddress(provinceCode, districtCode, wardCode, street) {
+	const province = provinces.find(
+		(item) => item.value === provinceCode
+	).label;
+	const district = districts[provinceCode].find(
+		(item) => item.value === districtCode
+	).label;
+	const ward = wards[districtCode].find(
+		(item) => item.value === wardCode
+	).label;
+
+	return `${street}, ${ward}, ${district}, ${province}`;
+}
+
+function decodeAddress(address) {
+	if (!address) return {};
+	const [street, ward, district, province] = address.split(", ");
+	const provinceCode = provinces.find(
+		(item) => item.label === province
+	).value;
+	const districtCode = districts[provinceCode].find(
+		(item) => item.label === district
+	).value;
+	const wardCode = wards[districtCode].find(
+		(item) => item.label === ward
+	).value;
+	return { provinceCode, districtCode, wardCode, street };
+}
+
+export {
+	generateRoute,
+	vietNamDong,
+	handleError,
+	generateAddress,
+	decodeAddress,
+};
