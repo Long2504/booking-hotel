875d452536b9ef36fa9823526377d6b662c5646a
Long2504 <thanhlong25042001@gmail.com>
2024-07-19 21:57:11 +0700

    setup client_id to login by gg

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,18 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+//files
 import "./index.css";
 import "./assets/styles/index.scss";
 import App from "./App";
+import { store } from "./redux/store";
+
+//libs
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<Router>
-				<GoogleOAuthProvider clientId='1059722329790-ggq09ntvloiqd194dgij5ahre7su77l0.apps.googleusercontent.com'>
+				<GoogleOAuthProvider clientId={googleClientId}>
 					<App />
 				</GoogleOAuthProvider>
 			</Router>
