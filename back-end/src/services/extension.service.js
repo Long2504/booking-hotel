675aea959559061db2c33f9e0e9cc4edd7895e2a
Long2504 <thanhlong25042001@gmail.com>
2024-07-19 22:00:25 +0700

    add field for api get all extension be

--- back-end/src/services/extension.service.js
+++ back-end/src/services/extension.service.js
@@ -5,41 +5,41 @@ import BaseService from "./base.service.js";
 import { Op } from "sequelize";
 
 class ExtensionService extends BaseService {
-    async getAll(options) {
-        const whereCondition = {};
-        if (options.searchQuery) {
-            whereCondition.name = {
-                [Op.like]: `%${options.searchQuery}%`,
-            };
-        }
-        options = {
-            where: whereCondition,
-            limit: options.pageSize,
-            offset: (options.page - 1) * options.pageSize,
-            attributes: ["id", "name", "createdAt", "description"],
-            include: [
-                {
-                    model: SubExtensionModel,
-                    as: "subExtensions",
-                    attributes: ["id", "name"],
-                },
-            ],
-            order: [["description", "DESC"]],
-        };
-        const { count, rows } = await this.getAndCountAll(options);
-        return {
-            list: rows,
-            total: count,
-        };
-    }
+	async getAll(options) {
+		const whereCondition = {};
+		if (options.searchQuery) {
+			whereCondition.name = {
+				[Op.like]: `%${options.searchQuery}%`,
+			};
+		}
+		options = {
+			where: whereCondition,
+			limit: options.pageSize,
+			offset: (options.page - 1) * options.pageSize,
+			attributes: ["id", "name", "createdAt", "description"],
+			include: [
+				{
+					model: SubExtensionModel,
+					as: "subExtensions",
+					attributes: ["id", "name", "createdAt"],
+				},
+			],
+			order: [["description", "DESC"]],
+		};
+		const { count, rows } = await this.getAndCountAll(options);
+		return {
+			list: rows,
+			total: count,
+		};
+	}
 
-    async remove(id) {
-        const data = this.deleteById(id);
-        if (data) {
-            return data;
-        }
-        throw new NotFoundError("Extension not found");
-    }
+	async remove(id) {
+		const data = this.deleteById(id);
+		if (data) {
+			return data;
+		}
+		throw new NotFoundError("Extension not found");
+	}
 }
 
 export default new ExtensionService(ExtensionModel);
