b912a660d62520716b95bfef0149d90eca264dbf
Long2504 <thanhlong25042001@gmail.com>
2024-07-21 11:51:29 +0700

    init data for list hotel be

--- back-end/src/services/hotel.service.js
+++ back-end/src/services/hotel.service.js
@@ -10,111 +10,128 @@ import HotelRoomService from "./hotelRoom.service.js";
 import subExtensionService from "./subExtension.service.js";
 
 class HotelService extends BaseService {
-    async createAndPost(data) {
-        const {
-            name,
-            description,
-            address,
-            images,
-            star,
-            hostId,
-            extensions,
-            rooms,
-        } = data;
-        const hotel = await this.create({
-            name,
-            description,
-            address,
-            images,
-            star,
-            hostId,
-            idPost: true,
-        });
-        await HotelExtensionService.createBulkHotelExtension(
-            extensions,
-            hotel.id
-        );
-        await HotelRoomService.createBulkRoomForHotel(hotel.id, rooms);
+	async initHotel(listHotel) {
+		try {
+			const count = await this.count();
+			if (count > 0) {
+				return;
+			}
+			const promise = listHotel.map(async (hotel) => {
+				await this.create(hotel);
+			});
 
-        return hotel;
-    }
+			await Promise.all(promise);
+			return;
+		} catch (error) {
+			console.log("🚀 ~ file: hotel.service.js:26 ~ error:");
+		}
+	}
 
-    async getAll(options) {
-        const whereCondition = {};
-        if (options.searchQuery) {
-            whereCondition.name = {
-                [Op.like]: `%${options.searchQuery}%`,
-            };
-        }
-        options = {
-            where: whereCondition,
-            limit: options.pageSize,
-            offset: (options.page - 1) * options.pageSize,
-            attributes: {
-                exclude: ["idPost", "updatedAt", "deletedAt"],
-            },
-            include: [
-                {
-                    model: HotelExtensionModel,
-                    as: "hotelExtensions",
-                    attributes: ["id", "extensionId", "subExtensions"],
-                    include: [
-                        {
-                            model: ExtensionModel,
-                            as: "extension",
-                            attributes: ["id", "name"],
-                        },
-                    ],
-                },
-                {
-                    model: HotelRoomModel,
-                    as: "hotelRooms",
-                    attributes: {
-                        exclude: [
-                            "hotelId",
-                            "roomTypeId",
-                            "updatedAt",
-                            "deletedAt",
-                        ],
-                    },
-                    include: {
-                        model: RoomsBedModel,
-                        as: "roomsBeds",
-                        attributes: ["id", "numBeds"],
-                        include: {
-                            model: BedsTypeModel,
-                            as: "bedType",
-                            attributes: ["id", "name"],
-                        },
-                    },
-                },
-            ],
-            order: [["createdAt", "DESC"]],
-            distinct: true,
-        };
-        const { count, rows } = await this.getAndCountAll(options);
-        const listHotel = rows.map(async (row) => {
-            const listExtension = row?.hotelExtensions?.map(
-                async (hotelExtension) => {
-                    if (hotelExtension?.subExtensions?.length > 0) {
-                        const listSubExtension =
-                            await subExtensionService.getListByListId(
-                                hotelExtension.subExtensions
-                            );
-                        hotelExtension.subExtensions = listSubExtension;
-                    }
-                    return hotelExtension;
-                }
-            );
-            row.hotelExtensions = await Promise.all(listExtension);
-            return row;
-        }); 
-        
-        return {
-            list: await Promise.all(listHotel),
-            total: count,
-        };
-    }
+	async createAndPost(data) {
+		const {
+			name,
+			description,
+			address,
+			images,
+			star,
+			hostId,
+			extensions,
+			rooms,
+		} = data;
+		const hotel = await this.create({
+			name,
+			description,
+			address,
+			images,
+			star,
+			hostId,
+			idPost: true,
+		});
+		await HotelExtensionService.createBulkHotelExtension(
+			extensions,
+			hotel.id
+		);
+		await HotelRoomService.createBulkRoomForHotel(hotel.id, rooms);
+
+		return hotel;
+	}
+
+	async getAll(options) {
+		const whereCondition = {};
+		if (options.searchQuery) {
+			whereCondition.name = {
+				[Op.like]: `%${options.searchQuery}%`,
+			};
+		}
+		options = {
+			where: whereCondition,
+			limit: options.pageSize,
+			offset: (options.page - 1) * options.pageSize,
+			attributes: {
+				exclude: ["idPost", "updatedAt", "deletedAt"],
+			},
+			include: [
+				{
+					model: HotelExtensionModel,
+					as: "hotelExtensions",
+					attributes: ["id", "extensionId", "subExtensions"],
+					include: [
+						{
+							model: ExtensionModel,
+							as: "extension",
+							attributes: ["id", "name"],
+						},
+					],
+				},
+				{
+					model: HotelRoomModel,
+					as: "hotelRooms",
+					attributes: {
+						exclude: [
+							"hotelId",
+							"roomTypeId",
+							"updatedAt",
+							"deletedAt",
+						],
+					},
+					include: {
+						model: RoomsBedModel,
+						as: "roomsBeds",
+						attributes: ["id", "numBeds"],
+						include: {
+							model: BedsTypeModel,
+							as: "bedType",
+							attributes: ["id", "name"],
+						},
+					},
+				},
+			],
+			order: [["createdAt", "DESC"]],
+			distinct: true,
+		};
+		const { count, rows } = await this.getAndCountAll(options);
+		const listHotel = rows.map(async (row) => {
+			const listExtension = row?.hotelExtensions?.map(
+				async (hotelExtension) => {
+					if (hotelExtension?.subExtensions?.length > 0) {
+						const listSubExtension =
+							await subExtensionService.getListByListId(
+								hotelExtension.subExtensions
+							);
+						hotelExtension.subExtensions = listSubExtension;
+					}
+					return hotelExtension;
+				}
+			);
+			row.hotelExtensions = await Promise.all(listExtension);
+			return row;
+		});
+
+		return {
+			list: await Promise.all(listHotel),
+			total: count,
+		};
+	}
 }
 
 export default new HotelService(HotelModel);
--- back-end/src/services/hotelExtension.service.js
+++ back-end/src/services/hotelExtension.service.js
@@ -2,17 +2,20 @@ import HotelExtensionModel from "../models/hotelExtension.model.js";
 import BaseService from "./base.service.js";
 
 class HotelExtensionService extends BaseService {
-    async createBulkHotelExtension(data, idHotel) {
-        const listData = [];
-        for (const obj of data) {
-            listData.push({
-                extensionId: obj.id,
-                hotelId: idHotel,
-                subExtensions: obj.subExtension,
-            });
-        }
-        return await this.bulkCreate(listData);
-    }
+	async createBulkHotelExtension(data, idHotel) {
+		const listData = [];
+		for (const key in extension) {
+			if (Object.hasOwnProperty.call(extension, key)) {
+				const sub = extension[key];
+				listData.push({
+					extensionId: key,
+					hotelId: idHotel,
+					subExtensions: sub,
+				});
+			}
+		}
+		return await this.bulkCreate(listData);
+	}
 }
 
 export default new HotelExtensionService(HotelExtensionModel);
