751afab026b92b912bd9c522e906c4b657b2ed33
Long2504 <thanhlong25042001@gmail.com>
2024-07-23 19:57:55 +0700

    init router order be

--- back-end/src/routes/public/index.js
+++ back-end/src/routes/public/index.js
@@ -2,8 +2,10 @@
 import express from "express";
 import routerAuth from "./auth.public.js";
 import routerHotel from "./hotel.public.js";
+import routerOrder from "./order.public.js";
 const router = express.Router();
 
 router.use("/auth", routerAuth);
 router.use("/hotel", routerHotel);
+router.use("/order", routerOrder);
 export default router;
