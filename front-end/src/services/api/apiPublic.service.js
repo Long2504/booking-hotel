94146bd148a5d5d81d5d3eb88b19418f4102500b
Long2504 <thanhlong25042001@gmail.com>
2024-07-23 19:50:26 +0700

    update public api fe

--- front-end/src/services/api/apiPublic.service.js
+++ front-end/src/services/api/apiPublic.service.js
@@ -1,13 +1,9 @@
 import axios from "axios";
-import queryString from "query-string";
 
 const baseURL = "http://localhost:3055/api/public/";
 
 const publicApi = axios.create({
 	baseURL,
-	paramsSerializer: {
-		encode: (params) => queryString.stringify(params),
-	},
 });
 
 publicApi.interceptors.request.use(async (config) => {
