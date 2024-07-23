482d09f7b9c4aeba519fdf4ae4e42b5d8126dbbe
Long2504 <thanhlong25042001@gmail.com>
2024-07-23 19:58:23 +0700

    add func vietNamDong, addHoursToDate be

--- back-end/src/utils/index.utils.js
+++ back-end/src/utils/index.utils.js
@@ -1,11 +1,24 @@
 import _ from "lodash";
 
 const getInfoData = ({ fields = [], object = {} }) => {
-    return _.pick(object, fields);
+	return _.pick(object, fields);
 };
 
 const typeOf = (object) => {
-    return Object.prototype.toString.call(object).slice(8, -1);
+	return Object.prototype.toString.call(object).slice(8, -1);
 };
 
-export { getInfoData, typeOf };
+const vietNamDong = (money = 0) => {
+	return Intl.NumberFormat("vi", {
+		style: "currency",
+		currency: "VND",
+	}).format(money);
+};
+
+const addHoursToDate = (date, hours) => {
+	let newDate = new Date(date);
+	newDate.setHours(newDate.getHours() + hours);
+	return newDate;
+};
+
+export { getInfoData, typeOf, vietNamDong, addHoursToDate };
