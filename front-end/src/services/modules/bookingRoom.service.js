f98202ac786ced3cd6ffc7e54370a4bb62006aa6
Long2504 <thanhlong25042001@gmail.com>
2024-07-26 02:42:49 +0700

    api bookingRoom (getlist, getRoomisAvalable, createBooking, deleteBooking,....) fe

---
+++ front-end/src/services/modules/bookingRoom.service.js
@@ -0,0 +1,37 @@
+import handleApiCall from "../api/apiCommon.service";
+import privateApi from "../api/apiPrivate.service";
+
+const bookingRoomEndpoint = {
+	getListForHost: "booking-room/host",
+	createForHost: "booking-room/host",
+	getRoomsIsAvailableByHotelIdForHost: "booking-room/host/room-available",
+	deleteForHost: "booking-room/host",
+};
+
+const bookingRoomApi = {
+	getListForHost: async (params) => {
+		return await handleApiCall(() =>
+			privateApi.get(bookingRoomEndpoint.getListForHost, { params })
+		);
+	},
+	getRoomsIsAvailableByHotelIdForHost: async (params) => {
+		return await handleApiCall(() =>
+			privateApi.get(
+				bookingRoomEndpoint.getRoomsIsAvailableByHotelIdForHost,
+				{ params }
+			)
+		);
+	},
+	createForHost: async (data) => {
+		return await handleApiCall(() =>
+			privateApi.post(bookingRoomEndpoint.createForHost, data)
+		);
+	},
+	deleteForHost: async (id) => {
+		return await handleApiCall(() =>
+			privateApi.delete(`${bookingRoomEndpoint.deleteForHost}/${id}`)
+		);
+	},
+};
+
+export default bookingRoomApi;
