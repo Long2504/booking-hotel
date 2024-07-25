58ba49ca8e58c503b89ba744b3b452b4e8a47d3e
Long2504 <thanhlong25042001@gmail.com>
2024-07-26 02:50:10 +0700

    get hotel finished for host fe

--- front-end/src/components/host/hotel/ListHotelFinished.js
+++ front-end/src/components/host/hotel/ListHotelFinished.js
@@ -1,5 +1,13 @@
-import { Table, Pagination } from "antd";
+//files
 import Box from "../../common/box.core";
+import hotelApi from "../../../services/modules/hotel.service";
+import { handleError, vietNamDong } from "../../../utils/common.utils";
+
+//libs
+import { Table, Pagination, message } from "antd";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
 function ListHotelFinished() {
 	const columns = [
 		{
@@ -37,12 +45,37 @@ function ListHotelFinished() {
 			dataIndex: "priceAverage",
 			key: "priceAverage",
 			align: "center",
+			render: (priceAverage) => vietNamDong(priceAverage),
 		},
 	];
-	const listHotel = [];
+	const [listHotel, setListHotel] = useState([]);
+	const [totalPage, setTotalPage] = useState(0);
+	const [queryParams, setQueryParams] = useSearchParams();
+	const [messageApi, contextHolder] = message.useMessage();
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const params = {
+					page: parseInt(queryParams.get("page")) || 1,
+					pageSize: 10,
+					searchQuery: queryParams.get("searchQuery") || "",
+				};
+				const {
+					metaData: { list, total },
+				} = await hotelApi.getListForHost(params);
+				setListHotel(list);
+				setTotalPage(total);
+			} catch (error) {
+				const { errorMessage } = handleError(error);
+				messageApi.error(errorMessage);
+			}
+		})();
+	}, [queryParams, messageApi]);
 
 	return (
 		<Box border={false}>
+			{contextHolder}
 			<Table
 				dataSource={listHotel.map((item, index) => ({
 					...item,
@@ -51,7 +84,17 @@ function ListHotelFinished() {
 				columns={columns}
 				pagination={false}
 			/>
-			<Pagination style={{ textAlign: "right", marginTop: "20px" }} />
+			<Pagination
+				style={{ textAlign: "right", marginTop: "20px" }}
+				current={queryParams.get("page") || 1}
+				total={totalPage}
+				onChange={(page) => {
+					setQueryParams({
+						page: parseInt(page),
+						searchQuery: queryParams.get("searchQuery") || "",
+					});
+				}}
+			/>
 		</Box>
 	);
 }
