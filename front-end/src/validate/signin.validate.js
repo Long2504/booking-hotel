5760364132d613b9be13db54eea4965be9c3d290
Long2504 <thanhlong25042001@gmail.com>
2024-07-30 00:38:47 +0700

    validate signinForm Client,admn fe

--- front-end/src/validate/signin.validate.js
+++ front-end/src/validate/signin.validate.js
@@ -1,14 +1,19 @@
 import * as yup from "yup";
 
-const signinFormSchema = yup.object().shape({
-    email: yup
-        .string()
-        .email("Email is not valid")
-        .required("Email is required"),
-    password: yup
-        .string()
-        .min(8, "Password must be at least 8 characters")
-        .required("Password is required"),
+const signinClientFormSchema = yup.object().shape({
+	email: yup
+		.string()
+		.email("Email is not valid")
+		.required("Email is required"),
+	password: yup
+		.string()
+		.min(8, "Password must be at least 8 characters")
+		.required("Password is required"),
 });
 
-export default signinFormSchema;
+const signinAdminFormSchema = yup.object().shape({
+	email: yup.string().required("Email is required"),
+	password: yup.string().required("Password is required"),
+});
+
+export { signinClientFormSchema, signinAdminFormSchema };
