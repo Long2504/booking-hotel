9c18a296cfab41de30bb718f7aef2083d105f3a9
Long2504 <thanhlong25042001@gmail.com>
2024-07-30 00:41:25 +0700

    signout with admin fe

--- front-end/src/components/admin/common/Header.admin.js
+++ front-end/src/components/admin/common/Header.admin.js
@@ -1,16 +1,39 @@
 //files
 import ButtonCore from "../../common/button.core";
+import authApi from "../../../redux/action/authAction.redux";
+import { setIsAdmin } from "../../../utils/localStorage.utils";
+import { handleError } from "../../../utils/common.utils";
 
 //libs
 import { Header } from "antd/es/layout/layout";
-import { Avatar, Space, Dropdown } from "antd";
+import { Avatar, Space, Dropdown, message } from "antd";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 //icons
 import { LuUser2 } from "react-icons/lu";
 import { FaSortDown } from "react-icons/fa6";
 import { AiOutlineMenuFold, AiOutlineMenuUnfold } from "react-icons/ai";
 
+
 function HeaderAdmin({ collapsed, setCollapsed }) {
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const [messageApi, contextHolder] = message.useMessage();
+	const handleLogout = async () => {
+		try {
+			await dispatch(authApi.signOut()).unwrap();
+			setIsAdmin(false);
+			navigate("/admin/signin");
+		} catch (error) {
+			const { errorMessage } = handleError(error);
+			messageApi.open({
+				type: "error",
+				content: errorMessage,
+			});
+		}
+	};
+
 	const itemsDropdown = [
 		{
 			key: "1",
@@ -27,13 +50,15 @@ function HeaderAdmin({ collapsed, setCollapsed }) {
 		{
 			key: "4",
 			label: "Thoát",
+			onClick: () => handleLogout(),
 		},
 	];
 	const styleIcon = { color: "white", fontSize: "20px" };
 	return (
-		<Header className='header-admin'>
+		<Header className="header-admin">
+			{contextHolder}
 			<ButtonCore
-				type='text'
+				type="text"
 				icon={
 					collapsed ? (
 						<AiOutlineMenuUnfold style={styleIcon} />
@@ -42,11 +67,11 @@ function HeaderAdmin({ collapsed, setCollapsed }) {
 					)
 				}
 				onClick={() => setCollapsed(!collapsed)}
-				className='header-admin__collapsed'
+				className="header-admin__collapsed"
 			/>
-			<Space className='header-admin__user'>
+			<Space className="header-admin__user">
 				<Avatar
-					className='header-admin__user__avatar'
+					className="header-admin__user__avatar"
 					icon={<LuUser2 />}
 				/>
 				<Dropdown
@@ -57,7 +82,7 @@ function HeaderAdmin({ collapsed, setCollapsed }) {
 					overlayStyle={{ top: "60px" }}
 				>
 					<Space>
-						<span className='header-admin__user__name'>Admin</span>
+						<span className="header-admin__user__name">Admin</span>
 						<FaSortDown style={{ color: "white" }} />
 					</Space>
 				</Dropdown>
--- front-end/src/components/admin/common/Sider.admin.js
+++ front-end/src/components/admin/common/Sider.admin.js
@@ -1,16 +1,25 @@
 //files
 import { logoAdmin } from "../../../assets/images/index.image";
+import authApi from "../../../redux/action/authAction.redux";
+import { handleError } from "../../../utils/common.utils";
 
 //libs
 import Sider from "antd/es/layout/Sider";
-import { Menu } from "antd";
-import { Link } from "react-router-dom";
+import { Menu, message } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 //icons
 import { VscPieChart } from "react-icons/vsc";
 import { AiOutlineTeam, AiOutlineCarryOut } from "react-icons/ai";
 import { IoHomeOutline, IoSettingsOutline } from "react-icons/io5";
 import { CiLogout } from "react-icons/ci";
+import { setIsAdmin } from "../../../utils/localStorage.utils";
+
+
+
+
+
 function getItem(label, key, icon, children, type) {
 	return {
 		key,
@@ -21,49 +30,66 @@ function getItem(label, key, icon, children, type) {
 	};
 }
 function SiderAdmin({ collapsed }) {
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const [messageApi, contextHolder] = message.useMessage();
+	const handleLogout = async () => {
+		try {
+			await dispatch(authApi.signOut()).unwrap();
+			setIsAdmin(false);
+			navigate("/admin/signin");
+		} catch (error) {
+			const { errorMessage } = handleError(error);
+			messageApi.open({
+				type: "error",
+				content: errorMessage,
+			});
+		}
+	};
 	const items = [
 		getItem(
-			<Link to='/admin/dashboard'>Dashboard</Link>,
+			<Link to="/admin/dashboard">Dashboard</Link>,
 			"1",
 			<VscPieChart />
 		),
 		getItem(
-			<Link to='/admin/customers'>Khách hàng</Link>,
+			<Link to="/admin/customers">Khách hàng</Link>,
 			"2",
 			<AiOutlineTeam />
 		),
 		getItem(
-			<Link to='/admin/booking'>Các lịch đã đặt</Link>,
+			<Link to="/admin/booking">Các lịch đã đặt</Link>,
 			"3",
 			<AiOutlineCarryOut />
 		),
 		getItem(
-			<Link to='/admin/hotel'>Khách sạn</Link>,
+			<Link to="/admin/hotel">Khách sạn</Link>,
 			"4",
 			<IoHomeOutline />
 		),
 		getItem("Cài đặt", "sub2", <IoSettingsOutline />, [
-			getItem(<Link to='/admin/setting/extensions'>Tiện ích</Link>, "9"),
-			getItem(<Link to='/admin/setting/beds'>Loại Giường</Link>, "11"),
-			getItem(<Link to='/admin/setting/rooms'>Loại phòng</Link>, "12"),
+			getItem(<Link to="/admin/setting/extensions">Tiện ích</Link>, "9"),
+			getItem(<Link to="/admin/setting/beds">Loại Giường</Link>, "11"),
+			getItem(<Link to="/admin/setting/rooms">Loại phòng</Link>, "12"),
 		]),
 		getItem(
-			<div onClick={() => alert("Đăng xuất")}>Đăng xuất</div>,
+			<div onClick={() => handleLogout()}>Đăng xuất</div>,
 			"5",
 			<CiLogout />
 		),
 	];
 	return (
 		<Sider
-			className='sider-admin'
+			className="sider-admin"
 			trigger={null}
 			collapsible
 			collapsed={collapsed}
 		>
-			<div className='sider-admin__logo'>
-				<img src={logoAdmin} alt='logo' />
+			{contextHolder}
+			<div className="sider-admin__logo">
+				<img src={logoAdmin} alt="logo" />
 			</div>
-			<Menu mode='inline' defaultSelectedKeys={["1"]} items={items} />
+			<Menu mode="inline" defaultSelectedKeys={["1"]} items={items} />
 		</Sider>
 	);
 }
