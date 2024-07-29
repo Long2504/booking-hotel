58f9fa7ad3c62077c780dc99c6b398e027c2707d
Long2504 <thanhlong25042001@gmail.com>
2024-07-29 22:55:34 +0700

    api get hotel by code city fe

--- front-end/src/components/client/DestinationCarousels/TopHighlights.js
+++ front-end/src/components/client/DestinationCarousels/TopHighlights.js
@@ -1,50 +1,79 @@
 //files
 import HotelsHighlight from "./HotelsHighlight";
-import { listHotelHighlight } from '../../../data/listHotelHighlight.data';
+import hotelApi from "../../../services/modules/hotel.service";
 
 //libs
 import { Tabs } from "antd";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
 
 function TopHighlights() {
-	const listHotel = listHotelHighlight;
+	const [codeCity, setCodeCity] = useState("01");
+	const { filter } = useSelector((state) => state.hotel);
+
+	const [listHotel, setListHotel] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const params = {
+				page: 1,
+				pageSize: 8,
+				startDate: filter.startDate,
+				endDate: filter.endDate,
+				roomNumber: filter.roomNumber,
+				peopleNumber: filter.peopleNumber,
+				multiCity: [codeCity],
+			};
+			const {
+				metaData: { list },
+			} = await hotelApi.getList(params);
+			setListHotel(list);
+		})();
+	}, [codeCity, filter]);
 
 	const items = [
 		{
-			key: "1",
+			key: "01",
 			label: "Hà Nội",
 			children: <HotelsHighlight listHotel={listHotel} />,
 		},
 		{
-			key: "2",
+			key: "48",
 			label: "Đà Nẵng",
 			children: <HotelsHighlight listHotel={listHotel} />,
 		},
 		{
-			key: "3",
+			key: "77",
 			label: "Vũng Tàu",
 			children: <HotelsHighlight listHotel={listHotel} />,
 		},
 		{
-			key: "4",
+			key: "672",
 			label: "Đà Lạt",
 			children: <HotelsHighlight listHotel={listHotel} />,
 		},
 		{
-			key: "5",
+			key: "79",
 			label: "Hồ Chí Minh",
 			children: <HotelsHighlight listHotel={listHotel} />,
 		},
 	];
 
+	const onChangeTab = (key) => {
+		setCodeCity(key);
+	};
+
 	return (
-		<div className='top-highlights-landing'>
-			<div className='top-highlights-landing__header'>
+		<div className="top-highlights-landing">
+			<div className="top-highlights-landing__header">
 				Những chỗ nghỉ nổi bật được đề xuất cho quý khách:
 			</div>
 			<Tabs
 				items={items}
-				className='top-highlights-landing__content'
-				size='large'
+				className="top-highlights-landing__content"
+				size="large"
+				onChange={onChangeTab}
 			/>
 		</div>
 	);
